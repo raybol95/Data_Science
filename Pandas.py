75eb58fee04daff77c95074a652002125ea173e0
Aditya Raybol Saragih <53115305+raybol95@users.noreply.github.com>
2020-12-30 12:01:26 +0700

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -1,1 +1,5 @@
 import pandas as pd
+
+data = {'fruit':['apple', 'orange', 'grape', 'banana', 'pineapple'], 'value':[2,4,6,10,2]} # dictionary for column and row values
+df = pd.DataFrame(data) #make a dataframe from data dictionary
+df.head() #show first 5 rows of dataframe
